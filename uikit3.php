4c6991feebf189a514dd373ed3f85b60d77e00fd
Joomlaplates <71080509+Joomlaplates@users.noreply.github.com>
2022-03-12 16:58:46 +0100

    Add files via upload

--- uikit3.php
+++ uikit3.php
@@ -18,13 +18,13 @@ class plgSystemuikit3 extends JPlugin {
 
 		if($this->params->get('uikit') == 1) {
 			$document->addStyleSheet("media/uikit3/css/uikit.css", array(), array('async'=>'async'));
-			$document->addScript("media/uikit3/js/uikit.js", array(), array('async'=>'async'));
-			$document->addScript("media/uikit3/js/uikit-icons.js", array(), array('async'=>'async'));
+			$document->addScript("media/uikit3/js/uikit.min.js", array(), array('async'=>'async'));
+			$document->addScript("media/uikit3/js/uikit-icons.min.js", array(), array('async'=>'async'));
 		}
 		else {
 			$document->addStyleSheet('https://cdn.jsdelivr.net/gh/Joomlaplates/uikit3plugin@795e84ad5b13e35521786c93a14f1fe15099d869/css/uikit.css');
-			$document->addScript('https://cdn.jsdelivr.net/gh/Joomlaplates/uikit3plugin@795e84ad5b13e35521786c93a14f1fe15099d869/js/uikit.js');
-			$document->addScript('https://cdn.jsdelivr.net/gh/Joomlaplates/uikit3plugin@795e84ad5b13e35521786c93a14f1fe15099d869/js/uikit-icons.js');
+			$document->addScript('https://cdn.jsdelivr.net/gh/Joomlaplates/uikit3plugin@cd26a56e5637a91dcd88e5d5c82bd52f07ac1c0e/js/uikit.min.js');
+			$document->addScript('https://cdn.jsdelivr.net/gh/Joomlaplates/uikit3plugin@cd26a56e5637a91dcd88e5d5c82bd52f07ac1c0e/js/uikit-icons.min.js');
 		}
 
 	}
